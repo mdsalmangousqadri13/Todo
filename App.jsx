0b7b58a6748aeec1c3abbd63c1aa0420dc2e320b
md salman gous qadri <145575717+mdsalmangousqadri13@users.noreply.github.com>
2025-02-01 21:54:20 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import { Todo } from "./components/project/Todo/Todo";
+
+
+export const App = () => {
+  return(
+     <section className="container">
+        <Todo/>
+     </section>
+   );
+};
+
+    
